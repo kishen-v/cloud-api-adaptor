9b87584e5ded9ff988a5e5aa1e58a670d6ef4b73
Kishen Viswanathan <kishen.viswanathan@ibm.com>
2026-05-26 17:09:25 +0530

    Introduce code related to ibmcloud-powervs-e2e flow

---
+++ src/cloud-api-adaptor/test/provisioner/ibmcloud-powervs/provision_helm.go
@@ -0,0 +1,57 @@
+//go:build ibmcloud_powervs
+
+// (C) Copyright Confidential Containers Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package ibmcloud_powervs
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+
+	pv "github.com/confidential-containers/cloud-api-adaptor/src/cloud-api-adaptor/test/provisioner"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+)
+
+func NewIBMCloudPowerVSInstallChart(installDir, provider string) (pv.InstallChart, error) {
+	chartPath := filepath.Join(installDir, "charts", "peerpods")
+	namespace := pv.GetCAANamespace()
+	releaseName := "peerpods"
+	debug := false
+	// Convert provider name from ibmcloud_powervs to ibmcloudpowervs for helm values
+	providerName := strings.ReplaceAll(provider, "_", "")
+
+	helm, err := pv.NewHelm(chartPath, namespace, releaseName, providerName, debug)
+	if err != nil {
+		return nil, err
+	}
+
+	return &IBMCloudPowerVSInstallChart{
+		Helm: helm,
+	}, nil
+}
+
+type IBMCloudPowerVSInstallChart struct {
+	Helm *pv.Helm
+}
+
+func (ic *IBMCloudPowerVSInstallChart) GetHelm() *pv.Helm {
+	return ic.Helm
+}
+
+func (ic *IBMCloudPowerVSInstallChart) Install(ctx context.Context, cfg *envconf.Config) error {
+	return ic.Helm.Install(ctx, cfg)
+}
+
+func (ic *IBMCloudPowerVSInstallChart) Uninstall(ctx context.Context, cfg *envconf.Config) error {
+	return ic.Helm.Uninstall(ctx, cfg)
+}
+
+func (ic *IBMCloudPowerVSInstallChart) Configure(ctx context.Context, cfg *envconf.Config, properties map[string]string) error {
+	// Set provider-specific values from properties
+	for key, value := range properties {
+		ic.Helm.OverrideProviderValues[key] = value
+	}
+	return nil
+}
